07764fc2954234be1eef048d0970125db803f536
Ertan Turan <ertanturan@outlook.com>
2020-02-07 22:46:18 +0300

    slider component added

---
+++ Assets/CustomTools/EditorUtilityTool/Core/Code/UI Components/SliderComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SliderComponent : MonoBehaviour
+{
+    private UnityAction<float> _actionValueChange;
+
+    private Slider _slider;
+
+    protected virtual void Awake()
+    {
+        _actionValueChange += OnValueChange;
+
+        _slider = GetComponent<Slider>();
+        _slider.onValueChanged.AddListener(_actionValueChange);
+    }
+
+    protected virtual void OnValueChange(float value)
+    {
+        Debug.Log("Slider on value change -base- :" + value);
+    }
+
+}
