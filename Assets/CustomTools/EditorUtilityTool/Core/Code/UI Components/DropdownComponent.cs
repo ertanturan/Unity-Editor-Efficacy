864ae6518e8235ca24f2f3e0dee5b0680fe315cb
Ertan Turan <ertanturan@outlook.com>
2020-02-07 22:35:48 +0300

    dropdown component added

---
+++ Assets/CustomTools/EditorUtilityTool/Core/Code/UI Components/DropdownComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Dropdown))]
+public class DropdownComponent : MonoBehaviour
+{
+
+    private UnityAction<int> _actionValueChange;
+    private Dropdown _dropdown;
+
+    protected virtual void Awake()
+    {
+        _actionValueChange += OnValueChange;
+
+        _dropdown = GetComponent<Dropdown>();
+
+        _dropdown.onValueChanged.AddListener(_actionValueChange);
+    }
+
+    protected virtual void OnValueChange(int value)
+    {
+        Debug.Log("Dropdown value changed -base- : " + value);
+    }
+
+}
