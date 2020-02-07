d5bd3220dca4f97109da58098df123b41c333eb8
Ertan Turan <ertanturan@outlook.com>
2020-02-07 22:43:50 +0300

    scrollrect component added

---
+++ Assets/CustomTools/EditorUtilityTool/Core/Code/UI Components/ScrollComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollComponent : MonoBehaviour
+{
+
+    private UnityAction<Vector2> _actionValueChange;
+
+    private ScrollRect _scrollRect;
+
+
+    protected virtual void Awake()
+    {
+        _actionValueChange += OnValueChange;
+
+        _scrollRect.onValueChanged.AddListener(OnValueChange);
+    }
+
+    protected virtual void OnValueChange(Vector2 value)
+    {
+        Debug.Log("Scroll rect value changed -base- : " + value);
+    }
+
+}
