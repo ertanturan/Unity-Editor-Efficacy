7e7539bb9bc5a77e1c99ea3f2b13265ff7a6c5ff
Ertan Turan <ertanturan@outlook.com>
2020-02-07 22:26:32 +0300

    button component added

---
+++ Assets/CustomTools/EditorUtilityTool/Core/Code/UI Components/ButtonComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonComponent : MonoBehaviour
+{
+
+    private Button _button;
+
+    private UnityAction ActionButtonClick;
+
+    protected virtual void Awake()
+    {
+
+        ActionButtonClick += OnButtonClick;
+
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(ActionButtonClick);
+    }
+
+    protected virtual void OnButtonClick()
+    {
+        Debug.Log("Button Clicked -base-");
+    }
+
+}
