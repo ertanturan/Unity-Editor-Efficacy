602981cdcd7591a2967ac0b5fb7c18524788fca0
Ertan Turan <ertanturan@outlook.com>
2020-02-07 22:40:33 +0300

    inputfield component added

---
+++ Assets/CustomTools/EditorUtilityTool/Core/Code/UI Components/InputfieldComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class InputfieldComponent : MonoBehaviour
+{
+    private UnityAction<string> _actionValueChange;
+    private UnityAction<string> _actionValueEditEnd;
+
+    private InputField _field;
+
+
+    protected virtual void Awake()
+    {
+        _field = GetComponent<InputField>();
+
+        _actionValueChange += OnValueChange;
+        _actionValueEditEnd += OnEditEnd;
+
+        _field.onValueChanged.AddListener(_actionValueChange);
+        _field.onEndEdit.AddListener(_actionValueEditEnd);
+    }
+
+    protected virtual void OnValueChange(string text)
+    {
+        Debug.Log("Input field on value change -base- :" + text);
+    }
+
+    protected virtual void OnEditEnd(string text)
+    {
+        Debug.Log("Input field on edit end -base- :" + text);
+    }
+}
