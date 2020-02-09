1158cc362821bb133f2f11311999b86f4ab58253
Ertan Turan <ertanturan@outlook.com>
2020-02-09 16:31:50 +0300

    tmp dropdown component added

---
+++ Assets/CustomTools/EditorUtilityTool/Core/Code/UI Components/DropdownComponentTMP.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(TMP_Dropdown))]
+public class DropdownComponentTMP : MonoBehaviour
+{
+
+    private TMP_Dropdown _dd;
+    private UnityAction<int> _actionOnValueChange;
+
+    protected virtual void Awake()
+    {
+        _dd = GetComponent<TMP_Dropdown>();
+        _actionOnValueChange += OnValueChange;
+
+        _dd.onValueChanged.AddListener(_actionOnValueChange);
+    }
+
+    protected virtual void OnValueChange(int value)
+    {
+        Debug.Log("On value change -base- :" + value);
+    }
+
+}
